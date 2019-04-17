0253e3f454526e287935f4124456ec7a69316c9c
Ruslan Yuldashev <ruslan@dcode.team>
2019-04-18 00:26:02 +0500

    Answers for user

--- tg.py
+++ tg.py
@@ -6,39 +6,21 @@ Here is the module for testing functions and telegram integration
 import telegram
 bot = telegram.Bot(token='656974486:AAFhijLPV10bnge-n8buRhRxKsfpaDYP43I')  # it's a private! Don't steal it!
 
-# just getting info about bot
-print(bot.get_me())
 
-
-# getting updates - unread messages
-updates = bot.get_updates()
-print([u.message.text for u in updates])
-
-
-
-def send_text_to_me(text):
+def send_text_to_me():
     # sending message to me
-    # chat_id = bot.get_updates()[-1].message.chat_id  # тот, с кем у бота была переписка
     chat_id = 82493329
+
+    last_message = bot.get_updates()[-1].message.text
+
+    if last_message == '1':
+        text = 'Вы прислали мне цифру один'
+    elif last_message == '2':
+        text = '<b>TWO IS MY FAVORITE</b>'
+    else:
+        text = 'Вы мне прислали такой текст: «{}»'.format(last_message)
+
     bot.send_message(chat_id=chat_id, text=text, parse_mode='HTML')
 
 
-# списки
-list1 = (1, 2, 3, 4, )
-
-
-# словари
-dict1 = {
-    2: 20000,
-    4: 40000
-}
-
-for number in list1:
-    status = 'нет значения'
-    if number in dict1:
-        status = dict1[number]
-    send_text_to_me('Наше число «{}» - {}'.format(number, status))
-
-# print(list1)
-
-
+send_text_to_me()
